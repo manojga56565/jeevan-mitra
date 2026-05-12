2707e8d103ef6855ef770a564604ee31355a7b00
manojga56565 <146167789+manojga56565@users.noreply.github.com>
2026-05-12 10:44:35 +0530

    Update server.js

--- jeevan-mitra/backend/server.js
+++ jeevan-mitra/backend/server.js
@@ -14,7 +14,11 @@ const io = new Server(server, {
 app.set('io', io);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
